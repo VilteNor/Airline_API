f679aa02689753f690453641a96b514ab3f92ede
Vilte N <viltenor@gmail.com>
2022-09-09 11:28:02 +0100

    added constructor, getters, setters

--- src/main/java/com/example/airline_api_lab/models/Passenger.java
+++ src/main/java/com/example/airline_api_lab/models/Passenger.java
@@ -3,6 +3,7 @@ package com.example.airline_api_lab.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -30,7 +31,43 @@ import java.util.List;
         private List<Flight> flights;
 
 
+        public Passenger(String name, String emailAddress) {
+            this.name = name;
+            this.emailAddress = emailAddress;
+            this.flights = new ArrayList<>();
+        }
 
+        public Passenger(){}
 
+        public long getId() {
+            return id;
+        }
 
-}
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmailAddress() {
+            return emailAddress;
+        }
+
+        public void setEmailAddress(String emailAddress) {
+            this.emailAddress = emailAddress;
+        }
+
+        public List<Flight> getFlights() {
+            return flights;
+        }
+
+        public void setFlights(List<Flight> flights) {
+            this.flights = flights;
+        }
+    }
